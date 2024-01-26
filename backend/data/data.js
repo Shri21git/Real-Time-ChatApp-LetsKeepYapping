37b140cf73943406ef2dfdf10bba3ce9b242ddbe
Shri21git <shriji8th2@gmail.com>
2024-01-26 20:44:25 +0530

    server created

---
+++ backend/data/data.js
@@ -0,0 +1,34 @@
+const chats = [
+  {
+    isGrpChat: false,
+    users: [
+      {
+        name: "sam",
+        email: "sam@123.com",
+      },
+      {
+        name: "john",
+        email: "john@abc.in",
+      },
+    ],
+    _id: "123",
+    chatName: "hello",
+  },
+  {
+    isGrpChat: false,
+    users: [
+      {
+        name: "joe",
+        email: "joe@123.com",
+      },
+      {
+        name: "john",
+        email: "john@abc.in",
+      },
+    ],
+    _id: "128",
+    chatName: "nah bros",
+  },
+];
+
+export default chats;
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import chats from "./data/data.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.get("/api/chats", (req, res) => {
+  //   console.log(chats);
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  //   console.log(singleChat);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on Port ${PORT}`));
